d58dd65b810638d1a85da80618f13785fec3c620
MrSpoo <63648131+MrSpoo@users.noreply.github.com>
2022-01-29 18:53:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const bedrock = require('bedrock-protocol')
 const client = bedrock.createClient({
   host: 'AREA0098.aternos.me',   // optional
   port: 31463,         // optional, default 19132
-  username: '1',   // the username you want to join as, optional if online mode
+  username: '69',   // the username you want to join as, optional if online mode
+  version: '1.17.40',   
   offline: true       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
 })
 
